7ba95815572e3fb7ba47684b593afa6d6854ef55
evicsouza <evavic.ifpe@gmail.com>
2023-06-28 20:36:48 -0300

    adicionando correção na home

--- src/componets/Cards/CardCalendario.js
+++ src/componets/Cards/CardCalendario.js
@@ -1,24 +1,3 @@
-// import * as React from 'react';
-// import Typography from '@mui/material/Typography';
-// import { DateCalendar } from '@mui/x-date-pickers';
-// import MenuSuperior from '../MenuSuperior';
-
-
-// export default function CardCalendario() {
-//   return (
-//     <React.Fragment>
-//       <MenuSuperior>Calendario</MenuSuperior>
-//       <Typography component="p" variant="h4">
-//       </Typography>
-//       <Typography color="text.secondary" sx={{ flex: 1 }}>
-//       </Typography>
-//       <div>
-//       <DateCalendar />
-//       </div>
-//     </React.Fragment>
-//   );
-// }
-
 import * as React from 'react';
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
