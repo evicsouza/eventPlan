dec2cc5717e2a55be9205a6020864a7b5a511edc
evicsouza <evavic.ifpe@gmail.com>
2023-07-04 17:48:31 -0300

    corrigindo o login

--- src/componets/Login.js
+++ src/componets/Login.js
@@ -9,6 +9,8 @@ import Paper from '@mui/material/Paper';
 import Box from '@mui/material/Box';
 import Grid from '@mui/material/Grid';
 import Typography from '@mui/material/Typography';
+import axios from 'axios';
+import { useState } from 'react';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 
 const theme = createTheme();
@@ -61,9 +63,9 @@ export default function Login() {
                         <Typography component="h1" variant="h5">
                             Entrar
                         </Typography>
-                        <Box component="form" noValidate onSubmit={fazerLogin} sx={{ mt: 1 }}>
+                        <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 1 }}>
                             <TextField margin="normal" required fullWidth id="email" label="Email" name="email" autoComplete="email" autoFocus onChange={(event) => setEmail(event.target.value)}/>
-                            <TextField margin="normal" required fullWidth id="senha" label="Senha" name="senha" autoComplete="current-senha" onChange={(event) => setSenha(event.target.value)}/>
+                            <TextField margin="normal" required fullWidth id="senha" label="Senha" name="senha" autoComplete="current-senha" onChange={(event) => setPassword(event.target.value)}/>
                             <FormControlLabel
                                 control={<Checkbox value="remember" color="primary" />}
                                 label="Lembre-se de mim"
